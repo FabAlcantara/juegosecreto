dcd119119c924b7dd58eafc743a73f891fce440a
FabAlcantara <158119817+FabAlcantara@users.noreply.github.com>
2024-01-29 20:15:14 -0600

    Version final juego numero secreto

---
+++ app.js
@@ -0,0 +1,40 @@
+//Variables
+let numeroMaximoPosible = 100;
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
+let numeroUsuario = 0;
+let intentos = 1;
+//let palabraVeces = "vez";
+let maximosIntentos = 10;
+
+
+while (numeroUsuario != numeroSecreto) {
+
+    numeroUsuario = parseInt(prompt(`Me indicas un numero entre 1 y ${numeroMaximoPosible} porfavor:`));
+
+    console.log(typeof(numeroUsuario));
+    /*
+    Este codigo realiza
+    la comparacion
+    */
+    if (numeroUsuario == numeroSecreto){
+        //Acertamos, fue verdadera la condicion
+        alert(`Acertaste, el numero es: ${numeroUsuario}, lo hiciste en ${intentos} ${intentos == 1 ? "vez" : "veces" }`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert("El numero secreto es menor");
+        } else {
+            alert("El numero secreto es mayor");
+        }
+        //Incrementamos el contador cuando no acierta
+        //intentos = intentos + 1;
+        //intentos += 1;
+        intentos ++;
+        //palabraVeces = "veces";
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al numero maximo de ${maximosIntentos} intentos`);
+            break;
+        }
+        //La condicion, no se cumplio
+        //alert("Lo siento no acertaste el numero");
+    }
+}
